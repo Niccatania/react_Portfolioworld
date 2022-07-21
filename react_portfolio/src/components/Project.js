424f27ca84963521686dda1b8f11bdeb39fe535a
Niccatania <Niccatania6@gmail.com>
2022-07-21 04:02:41 -0700

    added project images

--- react_portfolio/src/components/Project.js
+++ react_portfolio/src/components/Project.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from './Header';
+import pwa from './assets/pwa.png'
+import Calendar from './assets/Calendar.png'
+import Dinner from './assets/DDscreenshot.png'
+import Note from './assets/noteTaker.png'
+import Move from './assets/profile.jpeg'
+import Weather from './assets/weatherAPP.png'
 
 export default function Project() {
-    return(
-        <div>
-            <p>This a project component- Here We will Put projects
-        
-                </p>
+
+
+    const iconClass={
+        maxWidth: '50%',
+    };
+
+    return (
+    <div>
+    <img style= {iconClass} src={pwa}/>
+    <img style= {iconClass} src={Calendar}/>
+    <img style= {iconClass} src={Dinner}/>
+    <img style= {iconClass} src={Note}/>
+    <img style= {iconClass} src={Move}/>
+    <img style= {iconClass} src={Weather}/>
+
 
         </div>
     );
