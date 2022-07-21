6cc63c9557ebf032f824a92465fb53b0ea74c299
Niccatania <Niccatania6@gmail.com>
2022-07-21 03:51:34 -0700

    Adding footer icons

--- react_portfolio/src/components/Footer.js
+++ react_portfolio/src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 export default function Footer(){
+    const iconClass={
+        maxWidth: '10%',
+    };
+
     return (
     <div>
+        <img style= {iconClass} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" href="https://github.com/Niccatania"  />
         <a href="https://github.com/Niccatania" >Github </a>
+
+        <img style={iconClass} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" href="https://www.linkedin.com/in/niccatania/" />
+          
         <a href="https://www.linkedin.com/in/niccatania/" >Linkedin </a>
-        <a href="https://www.youtube.com/channel/UCP7myUng4vPupmbeTWYLiGg"> Youtube</a>
+
+
+        <img style={iconClass} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg"  href="https://twitter.com/NicCatania"/>
+          
+        <a href="https://twitter.com/NicCatania"> Twitter</a>
     </div>
     )
 };
